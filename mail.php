6b5e8f6e7b1c8da3be772268960538e208456ff0
Bchrecrutement <106110855+Bchrecrutement@users.noreply.github.com>
2022-05-26 16:23:21 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,89 @@
 <?php
 
-$Nom = $_POST['Nom'];
-$Prenom = $_POST['Prenom'];
-$email = $_POST['email'];
+error_reporting(E_ALL); 
+ini_set("display_errors", 1); //Affichage des erreurs
+
+//Eviter les insertions de scripts dans le cas d'un e-mail HTML
+$Nom = htmlentities($_POST['Nom']);
+$Prenom = htmlentities($_POST['Prenom']);
+$email_from = htmlentities($_POST['email']); 
+$Diplomes = htmlentities($_POST['Diplomes']);
+$message = htmlentities($_POST['message']);
+
+//Verifie si le fournisseur prend en charge les r
+if(preg_match("#@(hotmail|live|msn).[a-z]{2,4}$#", $email_from)){
+    $passage_ligne = "\n";
+}else{
+    $passage_ligne = "\r\n";
+}
+
+$email_to = "mlb.hdc@gmail.com"; //Destinataire
+$email_subject = "Nouveau Candidat"; //Sujet du mail
+$boundary = md5(rand()); // clé aléatoire de limite
+
+function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+}
+
+$headers = "From: ".$Nom." <".$email_from.">" . $passage_ligne; //Emetteur
+$headers.= "Reply-to: ".$Nom." <".$email_from.">" . $passage_ligne; //Emetteur
+$headers.= "MIME-Version: 1.0" . $passage_ligne; //Version de MIME
+$headers.= 'Content-Type: multipart/mixed; boundary='.$boundary .' '. $passage_ligne; 
+//Contenu du message (alternative pour deux versions ex:text/plain et text/html
+
+$email_message = '--' . $boundary . $passage_ligne; //Séparateur d'ouverture
+$email_message .= "Content-Type: text/plain; charset=utf-8" . $passage_ligne; //Type du contenu
+$email_message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne; //Encodage
+$email_message .= $passage_ligne .clean_string($message). $passage_ligne; //Contenu du message
+
+//Pièce jointe
+    if(isset($_FILES["fichier"]) &&  $_FILES['fichier']['name'] != ""){ //Vérifie sur formulaire envoyé et que le fichier existe
+        $nom_fichier = $_FILES['fichier']['name'];
+        $source = $_FILES['fichier']['tmp_name'];
+        $type_fichier = $_FILES['fichier']['type'];
+        $taille_fichier = $_FILES['fichier']['size'];
+                    
+        if($nom_fichier != ".htaccess"){ //Vérifie que ce n'est pas un .htaccess
+if($type_fichier == "image/jpeg" 
+                || $type_fichier == "image/pjpeg" 
+                || $type_fichier == "application/pdf"){ //Soit un jpeg soit un pdf
+                 
+                if ($taille_fichier <= 2097152) { //Taille supérieure à Mo (en octets)
+                    $tabRemplacement = array("é"=>"e", "è"=>"e", "à"=>"a"); //Remplacement des caractères spéciaux
+                    
+                    $handle = fopen($source, 'r'); //Ouverture du fichier
+                    $content = fread($handle, $taille_fichier); //Lecture du fichier
+                    $encoded_content = chunk_split(base64_encode($content)); //Encodage
+                    $f = fclose($handle); //Fermeture du fichier
+                                
+                    $email_message .= $passage_ligne . "--" . $boundary . $passage_ligne; //Deuxième séparateur d'ouverture
+                    $email_message .= 'Content-type:'.$type_fichier.';name="'.$nom_fichier.'"'. $passage_ligne; //Type de contenu (application/pdf ou image/jpeg)
+                    $email_message .='Content-Disposition: attachment; filename="'.$nom_fichier.'"'. $passage_ligne; //Précision de pièce jointe
+                    $email_message .= 'Content-transfer-encoding:base64'. $passage_ligne; //Encodage
+                    $email_message .= $passage_ligne; //Ligne blanche. IMPORTANT !
+                    $email_message .= $encoded_content. $passage_ligne; //Pièce jointe
+
+                }else{
+//Message d'erreur
+                    $email_message .= $passage_ligne ."L'utilisateur a tenté de vous envoyer une pièce jointe mais celle ci était superieure à 2Mo.". $passage_ligne;
+                }
+            }else{
+//Message d'erreur
+                $email_message .= $passage_ligne ."L'utilisateur a tenté de vous envoyer une pièce jointe mais elle n'était pas au bon format.". $passage_ligne;
+            }
+        }else{
+//Message d'erreur
+            $email_message .= $passage_ligne ."L'utilisateur a tenté de vous envoyer une pièce jointe .htaccess.". $passage_ligne;
+        }
+    }
+$email_message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne; //Séparateur de fermeture
+
+if(mail($email_to,$email_subject, $email_message, $headers)==true){  //Envoi du mail
+    header('Location: index.html'); //Redirection
+}
 
 
-$mailheader = "From:".$Nom."<".$email.">\r\n";
-
-$recipient = "mlb.hdc@gmail.com";
-
-mail($recipient, $Prenom, $mailheader) or die("Error!");
 
 echo'
 
