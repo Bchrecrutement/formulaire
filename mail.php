fb57e514002dcca9a91eb73a518004c4b555145a
Bchrecrutement <106110855+Bchrecrutement@users.noreply.github.com>
2022-05-26 16:29:25 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -84,31 +84,4 @@ if(mail($email_to,$email_subject, $email_message, $headers)==true){  //Envoi du 
 }
 
 
-
-echo'
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Merci pour votre candidature, nous l'avons bien reçue. Nous la traiterons le plus rapidement possible et reviendrons vers toi avec une réponse.!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
-    </div>
-</body>
-</html>
-
-
-
-';
-
-
 ?>
